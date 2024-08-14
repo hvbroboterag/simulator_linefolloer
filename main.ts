10fabe811adec47eab606228271aa308e79e1ec9
hvbroboterag <roboterag@wiebracht.de>
2024-08-14 15:47:46 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,25 @@
+basic.clearScreen()
 robot.tslMotionkit2.start()
-robot.setAssist(RobotAssist.LineFollowing, true)
+let LF_counter_mid = 0
+let LF_counter_left = 0
+let LF_counter_right = 0
 basic.forever(function () {
-    if (true) {
-    	
+    if (robot.obstacleDistance() < 10) {
+        robot.motorStop()
+        basic.showNumber(LF_counter_left)
     }
-})
-basic.forever(function () {
-    if (robot.detectLine(RobotLineDetector.Left) == true && robot.detectLine(RobotLineDetector.Right) == true) {
-        robot.motorSteer(0, 30)
-    } else if (robot.detectLine(RobotLineDetector.Left) == false && robot.detectLine(RobotLineDetector.Right) == true) {
-        robot.motorSteer(30, 20)
-    } else if (robot.detectLine(RobotLineDetector.Left) == true && robot.detectLine(RobotLineDetector.Right) == false) {
-        robot.motorSteer(-30, 20)
+    if (robot.obstacleDistance() >= 10) {
+        while (robot.detectLine(RobotLineDetector.Left) == true && robot.detectLine(RobotLineDetector.Right) == true) {
+            robot.motorSteer(0, 25, 50)
+            LF_counter_mid = LF_counter_left + 1
+        }
+        while (robot.detectLine(RobotLineDetector.Left) == false && robot.detectLine(RobotLineDetector.Right) == true) {
+            robot.motorSteer(25, 20, 50)
+            LF_counter_left = LF_counter_left + 1
+        }
+        while (robot.detectLine(RobotLineDetector.Left) == true && robot.detectLine(RobotLineDetector.Right) == false) {
+            robot.motorSteer(-25, 20, 50)
+            LF_counter_right = LF_counter_right + 1
+        }
     }
 })
